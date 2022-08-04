99678afd1d77dacc0287d69678dca169bb653685
Haogao <koohoko@gmail.com>
2022-08-04 18:35:09 +0800

    update data

--- scripts/clean_data.R
+++ scripts/clean_data.R
@@ -2,6 +2,7 @@ library(tidyverse)
 library(parallel)
 
 data_type <- c("hGEuIInf", "hGEuISti") # infection and stimulation
+
 for(this_type in data_type){
 	# this_type <- "hGEuIInf"
 	print(this_type)
@@ -12,11 +13,11 @@ for(this_type in data_type){
 	df_all <- df_all %>% select(-X1)
 	df_all <- df_all[!(is.na(df_all$entrezID) & is.na(df_all$gene_symbol)),] # remove na values
 
-	dir.create(paste0("../data/", this_type))
+	dir.create(paste0("../data_entrez_id/", this_type))
 	entrez_id_all <- unique(df_all$entrezID)
 	entrez_id_all <- entrez_id_all[!is.na(entrez_id_all)]
 	mclapply(entrez_id_all, function(x){
-		write_csv(df_all[which(df_all$entrezID==x),], paste0("../data/", this_type, "/", x, ".csv"))
+		write_csv(df_all[which(df_all$entrezID==x),], paste0("../data_entrez_id/", this_type, "/", x, ".csv"))
 	}, mc.cores = 8)
 }
 
@@ -30,10 +31,10 @@ for(this_type in data_type){
 	df_all <- df_all %>% select(-X1)
 	df_all <- df_all[!(is.na(df_all$entrezID) & is.na(df_all$gene_symbol)),] # remove na values
 
-	dir.create(paste0("../data/", this_type))
-	entrez_id_all <- unique(df_all$entrezID)
-	entrez_id_all <- entrez_id_all[!is.na(entrez_id_all)]
-	mclapply(entrez_id_all, function(x){
-		write_csv(df_all[which(df_all$entrezID==x),], paste0("../data/", this_type, "/", x, ".csv"))
+	dir.create(paste0("../data_gene_symbol/", this_type))
+	gene_symbol_all <- unique(df_all$gene_symbol)
+	gene_symbol_all <- gene_symbol_all[!is.na(gene_symbol_all)]
+	mclapply(gene_symbol_all, function(x){
+		write_csv(df_all[which(df_all$gene_symbol==x),], paste0("../data_gene_symbol/", this_type, "/", x, ".csv"))
 	}, mc.cores = 8)
 }
